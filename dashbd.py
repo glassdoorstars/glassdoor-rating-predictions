f53ddb39a78f399f1e4326a7c1159205bf4a0f4e
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-09-10 17:07:44 -0500

    add first viz

--- dashbd.py
+++ dashbd.py
@@ -1,13 +1,16 @@
+# Import necessary libraries
 import pandas as pd
-import wrangle
-import plotly.express as px  # (version 4.7.0 or higher)
+import plotly.express as px
 import plotly.graph_objects as go
-from dash import Dash, dcc, html, Input, Output  # pip install dash (version 2.0.0 or higher)
+from dash import Dash, dcc, html, Input, Output
 
 
-# Get data and transform
+# Import your custom module 'wrangle' (assuming it contains relevant data preprocessing functions)
+
 # --------------------------------------------------
-# tri-grams generation function
+# Data Preparation
+
+# Define a function to generate trigrams from lemmatized text
 def generate_trigrams(lemmatized):
     words = lemmatized.split()
     trigrams = []
@@ -23,39 +26,33 @@ def generate_trigrams(lemmatized):
     return trigrams
 
 
-# get data
+# Read data from CSV files
 three_star = pd.read_csv("data/three_star_tf_idf_scores.csv", index_col=0)
 four_star = pd.read_csv("data/four_star_tf_idf_scores.csv", index_col=0)
-# combine the two dataframes together
+
+# Combine the two dataframes together
 df = pd.concat([three_star, four_star], axis=0)
 
-# Initialize application
 # --------------------------------------------------
-# create a dash application
+# Initialize the Dash application
+
+# Create a Dash application instance
 app = Dash(__name__)
-# initilize app layout
+
+# Define the layout of the application
 app.layout = html.Div([
-    # set application title
-    html.H1(children='Glassdoor Companies Rating)', style={'text-align': 'center'}),
+    # Set application title
+    html.H1(children='Glassdoor Companies Rating', style={'text-align': 'center'}),
 
-    # # create drop down selection box for rating
-    #     dcc.Dropdown(id="select_rating",
-    #                  options=[{"label": "3-star", "value": 3},
-    #                           {"label": "4-star", "value": 4}],
-    #                  multi=False,
-    #                  value="3-star",
-    #                  style={'width': "50%"}),
-    #
-    #     # create drop down selection box for n-grams
-    #     dcc.Dropdown(id="select_n_grams",
-    #                      options=[{"label": "Unigram", "value": "unigram"},
-    #                             {"label": "bigram", "value": "bigram"},
-    #                               {"label": "trigram", "value": 'trigram'}],
-    #                      multi=False,
-    #                      value="unigram",
-    #                      style={'width': "50%"}),
+    # Dropdown for selecting the rating (3-star or 4-star)
+    dcc.Dropdown(id="select_rating",
+                 options=[{"label": "3-star", "value": 3},
+                          {"label": "4-star", "value": 4}],
+                 multi=False,
+                 value=3,
+                 style={'width': "50%"}),
 
-    # create drop down selection box for pros and cons
+    # Dropdown for selecting review type (Pros or Cons)
     dcc.Dropdown(id="select_review_type",
                  options=[{"label": "Pros", "value": "pros"},
                           {"label": "Cons", "value": 'cons'}],
@@ -63,27 +60,52 @@ app.layout = html.Div([
                  value="pros",
                  style={'width': "50%"}),
 
-    html.Div(id='pros_cons_container', children=[]),
+    # Container for displaying trigram word count data
+    html.Div(id='trigram_value_count_container', children=[]),
     html.Br(),
 
-    dcc.Graph(id='pros_cons_graph', figure={})
+    # Graph for displaying the trigram word count
+    dcc.Graph(id='trigram_value_count_graph', figure={})
 ])
 
 
-# Connect the Plotly graphs with Dash Components
-# -----------------------------------------------
-@app.callback([Output(component_id='pros_cons_container', component_property='children'),
-               Output(component_id='pros_cons_graph', component_property='figure')],
-              [Input(component_id='select_review_type', component_property='value')])
-def update_pros_cons_graph(select_review):
-    container = f"The  review type chosen by user was: {select_review}"
+# --------------------------------------------------
+# Define callback function to update the trigram word count graph
 
+@app.callback([Output(component_id='trigram_value_count_container', component_property='children'),
+               Output(component_id='trigram_value_count_graph', component_property='figure')],
+              [Input(component_id='select_review_type', component_property='value'),
+               Input(component_id='select_rating', component_property='value')])
+def update_trigram_value_count_graph(select_review, select_rating):
+    # Copy the original dataframe
     document = df.copy()
-    document = document[document["doc"] == select_review].head(20)
 
-    fig = px.bar(data_frame=document, x="word", y="count")
-    return container, fig
+    # Filter data based on selected review type and rating
+    document = document[(document["doc"] == select_review) & (document["rating"] == select_rating)].head(5)
 
+    # Create a bar chart using Plotly
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        x=document["count"],
+        y=document["word"],
+        orientation='h',
+        marker=dict(color='green'),
+    ))
+
+    # Update the layout of the bar chart
+    fig.update_layout(
+        title="Trigram Frequency by Review Type and Star Rating",
+        xaxis_title="Count",
+        yaxis_title="Word",
+        yaxis_categoryorder="total ascending",
+    )
+
+    # Return an empty list for the first output and the figure for the second output
+    return [], fig
+
+
+# --------------------------------------------------
+# Run the Dash application
 
 if __name__ == "__main__":
     app.run_server(debug=True)
